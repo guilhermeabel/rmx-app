8ca1e7ae04e45225d208f0d91c3025237c5c05e2
Guilherme Abel <guilhermeabel8@gmail.com>
2023-03-19 14:51:32 -0300

    add: sass + react query

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,49 @@
 import type { MetaFunction } from "@remix-run/node";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import styles from "~/styles/global.css";
+
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
 } from "@remix-run/react";
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: Infinity,
+		},
+	},
+});
+
+export function links() {
+	return [{ rel: "stylesheet", href: styles }];
+}
+
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+	charset: "utf-8",
+	title: "New Remix App",
+	viewport: "width=device-width,initial-scale=1",
 });
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<QueryClientProvider client={queryClient}>
+					<Outlet />
+					<ScrollRestoration />
+					<Scripts />
+					<LiveReload />
+				</QueryClientProvider>
+			</body>
+		</html>
+	);
 }
