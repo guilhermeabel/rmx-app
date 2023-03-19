0d65cf878f4cb742de59982845b8087a93433753
Guilherme Abel <guilhermeabel8@gmail.com>
2023-03-19 15:36:46 -0300

    refactor: extract products listing

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,5 @@
+import { Products } from "~/components/products";
+
 export default function Index() {
-	return (
-		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-			<h1>meu DEUS essa nicole é uma gata mesmo em puta que o pariu!!!!!</h1>
-			<ul>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/blog"
-						rel="noreferrer"
-					>
-						15m Quickstart Blog Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/jokes"
-						rel="noreferrer"
-					>
-						Deep Dive Jokes App Tutorial
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-						Remix Docs
-					</a>
-				</li>
-			</ul>
-		</div>
-	);
+	return <Products />;
 }
